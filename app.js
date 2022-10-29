313b2a84ef76d4100d79daecc0c4811bf1e75e0f
Ezequiel <ezequiel250891@hotmail.com>
2022-10-28 22:37:20 -0300

    1er y 2do puntos de la tarea, completa

--- app.js
+++ app.js
@@ -5,30 +5,28 @@ var express = require('express');
 var app = express();
 const port = 3000
 
-respond with "hello world" when a GET request is made to the homepage
+// respond with "hello world" when a GET request is made to the homepage
 app.get('/saludo', (req, res) => {
     const persona = {
         nombre: "Ezequiel",
         apellido: " Di Giacomo"
     }
         res.json({
-           saludo: `Hola yo soy ${persona.nombre} ${persona.apellido}`
+           msj: `Hola yo soy ${persona.nombre} ${persona.apellido}`
         })
 })
 
 
-app.get('/cuenta'), (req, res) => {
+app.get('/dividir/:divisor/:dividendo', (req, res) => {
 
-    const division = {
-        dividendo: " ",
-        divisor: ">=1"
+    if (req.params.divisor == 0) {
+         res.json ("error")
+        
+    }else {
+        res.json (req.params.dividendo / req.params.divisor)
     }
+})
 
-    if(req.params.divisor === 0){
-        res.json ("no se puede dividir por 0")
-    }
-    
-}
 
 app.listen(port, ()=> {
     console.log(`example app listen on port ${port}`)
